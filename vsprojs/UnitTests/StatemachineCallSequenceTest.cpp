38fe0253b4d07dbeab686a9f39b8b4866d545921
mrstefangrimm <motionssystems@grimm.com>
2022-09-15 20:58:23 +0200

    Add unit test.

--- vsprojs/UnitTests/StatemachineCallSequenceTest.cpp
+++ vsprojs/UnitTests/StatemachineCallSequenceTest.cpp
@@ -69,6 +69,7 @@ namespace UT {
 
       enum Triggers {
         A_B,
+        A_BA,
         B_A,
         BA_BB,
         BB_BA,
@@ -110,12 +111,14 @@ namespace UT {
       typedef Transition<Triggers::BA_BB, BA, BB, StateTypeCreationPolicyType, OkGuard, BA_BB_spy> BA_BB_t;
       typedef EntryDeclaration<Triggers::BB_A, BB, StateTypeCreationPolicyType, BB_B_spy> BB_B_t;
       typedef EntryDeclaration<Triggers::BBB_A, BB, StateTypeCreationPolicyType, BBB_B_spy> BBB_B_t;
+      typedef ExitTransition<Triggers::A_BA, A, BA, StateTypeCreationPolicyType, OkGuard, ActionSpy<A, BA>> A_BA_t;
       typedef
         Typelist<BB_BA_t,
         Typelist<BA_BB_t,
         Typelist<BB_B_t,
         Typelist<BBB_B_t,
-        NullType>>>> B_transitions;
+        Typelist<A_BA_t,
+        NullType>>>>> B_transitions;
       typedef InitialTransition<BA, StateTypeCreationPolicyType, B_Init_spy> B_initt;
       typedef Statemachine<
         B_transitions,
@@ -180,15 +183,17 @@ namespace UT {
       typedef Transition<Triggers::B_A, B, A, StateTypeCreationPolicyType, OkGuard, B_A_spy> B_A_t;
       typedef Transition<Triggers::A_B, A, B, StateTypeCreationPolicyType, OkGuard, A_B_spy> A_B_t;
       typedef Transition<Triggers::BB_A, B, A, StateTypeCreationPolicyType, OkGuard, BB_A_spy> BB_A_t;
-      typedef Transition<Triggers::BBB_A, B, A, StateTypeCreationPolicyType, OkGuard, BBB_A_spy> BBB_A_t;
-      typedef Declaration<Triggers::BB_BA, B, StateTypeCreationPolicyType> BB_BA_decl;
+      typedef Transition<Triggers::BBB_A, B, A, StateTypeCreationPolicyType, OkGuard, BBB_A_spy> BBB_A_d;
+      typedef Declaration<Triggers::BB_BA, B, StateTypeCreationPolicyType> BB_BA_d;
+      typedef ExitDeclaration<Triggers::A_BA, A, B, StateTypeCreationPolicyType, ActionSpy<A, BA>> A_BA_d;
       typedef
         Typelist<B_A_t,
         Typelist<A_B_t,
         Typelist<BB_A_t,
-        Typelist<BBB_A_t,
-        Typelist<BB_BA_decl,
-        NullType>>>>> Toplevel_transitions;
+        Typelist<BBB_A_d,
+        Typelist<BB_BA_d,
+        Typelist<A_BA_d,
+        NullType>>>>>> Toplevel_transitions;
 
       TEST_CLASS(StatemachineCallSequenceTests)
       {
@@ -369,40 +374,40 @@ namespace UT {
           Assert::AreEqual<size_t>(expected.size(), recorder.size());
         }
 
-        //TEST_METHOD(Callsequence_A__BA)
-        //{
-        //  typedef InitialTransition<B, StateTypeCreationPolicyType, ActionSpy<B, InitialStateFake>> ToplevelInitTransition;
-        //  typedef Statemachine<
-        //    Toplevel_transitions,
-        //    ToplevelInitTransition,
-        //    NullEndTransition<StateTypeCreationPolicyType>> Toplevel_sm;
+        TEST_METHOD(Callsequence_A__BA)
+        {
+          typedef InitialTransition<B, StateTypeCreationPolicyType, ActionSpy<B, InitialStateFake>> ToplevelInitTransition;
+          typedef Statemachine<
+            Toplevel_transitions,
+            ToplevelInitTransition,
+            NullEndTransition<StateTypeCreationPolicyType>> Toplevel_sm;
 
-        //  Toplevel_sm sm;
-        //  vector<string> expected;
-        //  recorder.clear();
+          Toplevel_sm sm;
+          vector<string> expected;
+          recorder.clear();
 
-        //  sm.begin();
-        //  expected.push_back("B<-Initial");
-        //  expected.push_back("B::Entry");
-        //  expected.push_back("BA<-Initial");
-        //  expected.push_back("BA::Entry");
-        //  expected.push_back("BA::Do");
+          sm.begin();
+          expected.push_back("B<-Initial");
+          expected.push_back("B::Entry");
+          expected.push_back("BA<-Initial");
+          expected.push_back("BA::Entry");
+          expected.push_back("BA::Do");
 
-        //  sm.dispatch<Triggers::A_BA>();
-        //  expected.push_back("A<-BA");
-        //  expected.push_back("BA::Exit");
-        //  expected.push_back("B::Exit");
-        //  expected.push_back("A::Entry");
-        //  expected.push_back("A::Do");
+          sm.dispatch<Triggers::A_BA>();
+          expected.push_back("A<-BA");
+          expected.push_back("BA::Exit");
+          expected.push_back("B::Exit");
+          expected.push_back("A::Entry");
+          expected.push_back("A::Do");
 
-        //  Assert::IsTrue(expected.size() >= recorder.size());
-        //  for (int n = 0; n < recorder.size(); n++) {
-        //    string exp = expected[n];
-        //    string rec = recorder[n];
-        //    Assert::AreEqual<string>(exp, rec);
-        //  }
-        //  Assert::AreEqual<size_t>(expected.size(), recorder.size());
-        //}
+          Assert::IsTrue(expected.size() >= recorder.size());
+          for (int n = 0; n < recorder.size(); n++) {
+            string exp = expected[n];
+            string rec = recorder[n];
+            Assert::AreEqual<string>(exp, rec);
+          }
+          Assert::AreEqual<size_t>(expected.size(), recorder.size());
+        }
 
       };
     }
