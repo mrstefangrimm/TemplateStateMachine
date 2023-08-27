b1a44e4d43b771a2601fd9e1cb1e1422901f4945
mrstefangrimm <motionssystems@grimm.com>
2023-08-27 13:28:08 +0200

    Rename TriggerExecutor to EventDispatcher and move to own file.

--- src/statemachine.h
+++ src/statemachine.h
@@ -16,6 +16,7 @@
 */
 #include "lokilight.h"
 #include "transition.h"
+#include "eventdispatchers.h"
 
 namespace tsmlib {
 
@@ -39,7 +40,7 @@ public:
     // Transitions can have initial transitions (for a higher-level state to a sub-state).
     // The default initial transition is added to the front and is therefore executed when no other was found.
     const int size = Length<Transitions>::value;
-    const auto result = Initializer< Transitions, Event, size - 1 >::init();
+    const auto result = Initializer< Transitions, Initialtransition, Event, size - 1 >::init();
     if (result.consumed) {
       activeState_ = result.activeState;
     }
@@ -82,7 +83,7 @@ public:
     if (activeState_ == nullptr) return DispatchResult<StateType>::null;
 
     const int size = Length<Transitions>::value;
-    auto result = TriggerExecutor< Event, size - 1 >::execute(activeState_, &ev);
+    auto result = EventDispatcher< Transitions, Event, size - 1 >::execute(activeState_, &ev);
 
     // Transition not found, active state is not changed
     if (!result.consumed) {
@@ -95,175 +96,5 @@ public:
 
 private:
   StateType* activeState_ = 0;
-
-public:
-  template<class Event, int Index>
-  struct TriggerExecutor {
-
-    static DispatchResult<StateType> execute(StateType* activeState, const Event* ev) {
-      // Finds last element in the list that meets the conditions.
-      using CurrentTransition = typename TypeAt<Transitions, Index>::Result;
-      using FromType = typename CurrentTransition::FromType::ObjectType;
-      using EventType = typename CurrentTransition::EventType;
-
-      const bool hasSameFromState = activeState->template typeOf<FromType>();
-
-      const bool conditionMet = is_same<EventType, Event>().value && hasSameFromState;
-      if (conditionMet) {
-        const EventType* currentTransitionEvent = reinterpret_cast<const EventType*>(ev);
-        auto result = CurrentTransition().dispatch(activeState, *currentTransitionEvent);
-        return result;
-      }
-      // Recursion
-      return TriggerExecutor< Event, Index - 1 >::execute(activeState, ev);
-    }
-
-    static void entry(StateType* entryState) {
-      // Finds last element in the list that meets the conditions.
-      using CurrentTransition = typename TypeAt<Transitions, Index>::Result;
-      using ToType = typename CurrentTransition::ToType::ObjectType;
-      using CreationPolicy = typename CurrentTransition::CreationPolicyType;
-
-      // TODO: Mustn't be a choice transition
-      //CompileTimeError < is_same<ToType, EmptyState<typename CreationPolicy::ObjectType>>().value >();
-
-      bool hasSameFromState = entryState->template typeOf<ToType>();
-
-      bool conditionMet = is_same<typename CurrentTransition::EventType, Event>().value&& hasSameFromState;
-      if (conditionMet) {
-        ToType* state = static_cast<ToType*>(entryState);
-        state->template _entry<Event>();
-        NullType nu;
-        state->_doit(nu);
-        return;
-      }
-      // Recursion
-      TriggerExecutor< Event, Index - 1 >::entry(entryState);
-    }
-  };
-  // Specialization
-  template<class Event>
-  struct TriggerExecutor<Event, 0> {
-
-    static DispatchResult<StateType> execute(StateType* activeState, const Event* ev) {
-      // End of recursion.
-      using FirstTransition = typename TypeAt<Transitions, 0>::Result;
-      using FromType = typename FirstTransition::FromType::ObjectType;
-      using EventType = typename FirstTransition::EventType;
-
-      const bool hasSameFromState = activeState->template typeOf<FromType>();
-
-      const bool conditionMet = is_same<typename FirstTransition::EventType, Event>().value && hasSameFromState;
-      if (conditionMet) {
-        const EventType* currentTransitionEvent = reinterpret_cast<const EventType*>(ev);
-        const auto result = FirstTransition().dispatch(activeState, *currentTransitionEvent);
-        return result;
-      }
-      return DispatchResult<StateType>(false, nullptr);
-    }
-
-    static void entry(StateType* entryState) {
-      // End of recursion.
-      using FirstTransition = typename TypeAt<Transitions, 0>::Result;
-      using ToType = typename FirstTransition::ToType::ObjectType;
-      using CreationPolicy = typename FirstTransition::CreationPolicyType;
-
-      const bool hasSameFromState = entryState->template typeOf<ToType>();
-
-      const bool conditionMet = is_same<typename FirstTransition::EventType, Event>().value && hasSameFromState;
-      if (conditionMet) {
-        ToType* state = static_cast<ToType*>(entryState);
-        state->template _entry<Event>();
-        NullType nu;
-        state->_doit(nu);
-      }
-      return;
-    }
-  };
-
-  template<class T, class Event, int Index>
-  struct Initializer {
-
-    static DispatchResult<StateType> init() {
-      using CurrentTransition = typename TypeAt<T, Index>::Result;
-      using EventType = typename CurrentTransition::EventType;
-      
-      if (CurrentTransition::E && is_same<EventType, Event>().value) {
-        EventType ev;
-        auto result = CurrentTransition().dispatch(nullptr, ev);
-        if (result.consumed) {
-          return result;
-        }
-      }
-      // Recursion
-      return Initializer< T, Event, Index - 1 >::init();
-    }
-  };
-  // Specialization
-  template<class T, class Event>
-  struct Initializer<T, Event, 0> {
-
-    static DispatchResult<StateType> init() {
-      // End of recursion.
-      using FirstTransition = typename TypeAt<T, 0>::Result;
-      using EventType = typename FirstTransition::EventType;
-
-      if (FirstTransition::E && is_same<EventType, Event>().value) {
-        EventType ev;
-        const auto result = FirstTransition().dispatch(nullptr, ev);
-        if (result.consumed) {
-          return result;
-        }
-      }
-      return Initialtransition().dispatch();
-    }
-  };
-
-  template<class T, int Index>
-  struct Finalizer {
-
-    static DispatchResult<StateType> end(StateType* activeState) {
-      using CurrentTransition = typename TypeAt<T, Index>::Result;
-      using FromType = typename CurrentTransition::FromType::ObjectType;
-
-      const bool hasSameFromState = activeState->template typeOf<FromType>();
-      const bool conditionMet = CurrentTransition::X && hasSameFromState;
-      if (conditionMet) {
-        using ET = typename CurrentTransition::EventType;
-        // TODO: Add event to _exit()?
-        ET ev{};
-        const auto result = CurrentTransition().dispatch(activeState, ev);
-        if (result.consumed) {
-          return result;
-        }
-      }
-      // Recursion
-      return Finalizer< T, Index - 1 >::end(activeState);
-    }
-  };
-  // Specialization
-  template<class T>
-  struct Finalizer<T, 0> {
-
-    static DispatchResult<StateType> end(StateType* activeState) {
-      // End of recursion.
-      using FirstTransition = typename TypeAt<T, 0>::Result;
-      using FromType = typename FirstTransition::FromType::ObjectType;
-
-      bool hasSameFromState = activeState->template typeOf<FromType>();
-
-      const bool conditionMet = FirstTransition::X && hasSameFromState;
-      if (conditionMet) {
-        using ET = typename FirstTransition::EventType;
-        // TODO: Add event to _exit()?
-        ET ev{};
-        const auto result = FirstTransition().dispatch(activeState, ev);
-        if (result.consumed) {
-          return result;
-        }
-      }
-      return DispatchResult<StateType>(false, activeState);
-    }
-  };
 };
 }
--- vsprojs/UnitTests/EventDispatchersTest.cpp
+++ vsprojs/UnitTests/EventDispatchersTest.cpp
@@ -26,7 +26,7 @@ namespace UT {
     using namespace Microsoft::VisualStudio::CppUnitTestFramework;
     using namespace tsmlib;
 
-    namespace TriggerExecutorImpl {
+    namespace EventDispatchersTestImpl {
 
       using StateType = State<VirtualGetTypeIdStateComparator, false>;
       using StateTypeCreationPolicyType = FactoryCreator<StateType, false>;
@@ -88,17 +88,14 @@ namespace UT {
         Typelist<ToOnFromOnTransition,
         Typelist<ToOffFromOffTransition,
         NullType>>>>;
-
-      using InitTransition = InitialTransition<OnState, StateTypeCreationPolicyType, NoAction>;
-      using Sm = Statemachine<TransitionList, InitTransition>;
     }
 
     // https://viewer.diagrams.net/?tags=%7B%7D&highlight=0000ff&layers=1&nav=1&title=TriggerExecutorTest.drawio#R7Vlbc%2BI2FP41zHQfyPiCCXkEAtmd2W3TkubSl46wha1GtlhZBJxfv0dYNrYlCJuYbDtdMgPS0cXWOZ%2B%2B80npuON4c8XRMvrCAkw7jhVsOu5lx3EGPQe%2BpSHLDeeOnRtCToLcVDHMyDNWRktZVyTAaa2jYIwKsqwbfZYk2Bc1G%2BKcrevdFozWn7pEIdYMMx9R3XpHAhGpZXnWzv4RkzAqnmxbqiVGRWdlSCMUsHXF5E467pgzJvJSvBljKn1X%2BCUfN93TWr4Yx4k4ZoBYX99gmonJ8NN6k0yer58mg66a5QnRlVpwGqvXFVnhA3jzpSyuYjrlKIbiaB0RgWdL5Ev7GiIPtkjEFGo2FDlbJQEOVK1cuQUVn8XEV2WK5piOkP8YbgeMGWUcmhKWyGekgrPH0utyogVLxBTFhEow3WIeoAQps0KODW4dIUrCBCo%2BeAbDhCPdVcW6MRd4UzEp111hFmPBM%2BiiWl0VxaxeXVcwUUA2quDBLToihcOwnHkXKiioaJkj9zfno8HDH%2FfWJf%2FLmXx0Hr%2FOcdceaKH7bbHQYocDgLOqMi4iFrIE0cnOWomVDMmuz2fGlsrv%2F2AhMuVhtBKsHmxwKs%2Fu1fht5UFWzryiermpNl5mRW1DxH0xB5Qro6C2GyQrxZi9kUzZivv4ANIVDwnEQywO%2BPUi7yf9dhAXCgi2l%2FfnmCJBnuqsYQq6mumaEVjBDl69Or6cfgM3%2BfrUqOoub0zUcweHJ8odcGCioiNbLFJc67PFaemCo6B7KBRV5CYacOsU8gLfIO4reDo9SQiE0oJMOo67gI9vlYxSa7Hg83qKOpKQZL3yVMtST9WISvIRgeQzVA1CbsE26Ms%2Br%2BNroPNX2aXKX2X2egt%2FGUEw%2BLFUtWOnhxo5manq9bRzodPOoT3xIu18H88MOUdZpcNS7vF0Pw05nleDiaru4pxP2CobXGhsoCODUtB7uB7LQpWk4FsxE0hgfeeXO23%2Fzm9FqrTNAwHhIGYJk0aAmNyirbBAv84CPZ0FXJOIaYEDBvRz77aXUb%2FnR5%2BWXzPvVzbv2q4W%2BxsSY1iwDoEkGEo5v%2FO5UW5WdUVRPkZX7NMwzksi5ihmMC%2B%2B1zI1qKB2C0lSiapniGphe6tyaWBKQ8vRysVuSOxm6tmjXPZz3Im1zcGoHqKzl8RMusxPsguykaBuY%2Bc7jfxvGw4wpvOLfarji876ptPLTxHYpghsng3eVQSaD7HW%2FyMBmBff9pFUhblrndnwqYX6nZOC11ZS6P%2FXksLBSP%2FLkoLnuD8sKfz%2BhT3ybHozev5zFnnTO765JYbbyLl83ZnAC5RA4YqTOO44MLn1y%2FgDfMOfm9cpAf5MwTOOJamAy30VYfgey9bRQ3c2hN%2FemSXHEsm0idxz0hgJIS%2BTh%2FLdnanP8Rb5wP8xS9IzxkOwqukBQdN51k0R%2FG7nmn7QAgnOF42TSi3jFFmjkp6UqcgEFC%2BEIQ%2FEJAjovsvXOgNWcs9FiRUNGAb4HH3%2Fea5DxbNMueNUWNG31A0nYYh5Hso%2BlS6cAw76oSzJGybNJhVH01jmn2bDHWdJ%2BH7RnjMhWHxUtFsIb%2FP60XAytE3htU%2FGBZ4uEPVLwve4MjKKifMj1UR56VQd9qKaaGT618mL77p5OiQmgGV7ZjHR8qW3ltxfe%2Bl9rEo4kQYw4vlcw7OZUH4eeTr1I8%2BbeK3fyFunPPJAdfff3Bw2u3%2BJu5Nv
-    TEST_CLASS(TriggerExecutorTest)
+    TEST_CLASS(EventDispatchersTest)
     {
       TEST_METHOD_INITIALIZE(Initialize)
       {
-        using namespace TriggerExecutorImpl;
+        using namespace EventDispatchersTestImpl;
         OnState::exitCalls = 0;
         OnState::entryCalls = 0;
         OnState::doitCalls = 0;
@@ -109,13 +106,13 @@ namespace UT {
 
       TEST_METHOD(Execute_ActiveStateOnTriggerTimeout_CallsOnStateDoit)
       {
-        using namespace TriggerExecutorImpl;
+        using namespace EventDispatchersTestImpl;
  
         OnState on;
         Trigger::Timeout t;
 
         const int size = Length<TransitionList>::value;
-        auto result = Sm::TriggerExecutor<Trigger::Timeout, size - 1>::execute(&on, &t);
+        auto result = EventDispatcher<TransitionList, Trigger::Timeout, size - 1>::execute(&on, &t);
         Assert::IsTrue(result.consumed);
         Assert::AreEqual<int>(on.getTypeId(), result.activeState->getTypeId());
 
@@ -125,13 +122,13 @@ namespace UT {
 
       TEST_METHOD(Execute_ActiveStateOnTriggerOn_UnhandledTransition)
       {
-        using namespace TriggerExecutorImpl;
+        using namespace EventDispatchersTestImpl;
 
         OnState on;
         Trigger::On t;
 
         const int size = Length<TransitionList>::value;
-        auto result = Sm::TriggerExecutor<Trigger::On, size - 1>::execute(&on, &t);
+        auto result = EventDispatcher<TransitionList, Trigger::On, size - 1>::execute(&on, &t);
         Assert::IsFalse(result.consumed);
         Assert::IsNull(result.activeState);
 
@@ -141,13 +138,13 @@ namespace UT {
 
       TEST_METHOD(Execute_WrongActiveStateTriggerTimeout_UnhandledTransition)
       {
-        using namespace TriggerExecutorImpl;
+        using namespace EventDispatchersTestImpl;
 
         WrongState wrongState;
         Trigger::Timeout t;
 
         const int size = Length<TransitionList>::value;
-        auto result = Sm::TriggerExecutor<Trigger::Timeout, size - 1>().execute(&wrongState, &t);
+        auto result = EventDispatcher<TransitionList, Trigger::Timeout, size - 1>().execute(&wrongState, &t);
         Assert::IsFalse(result.consumed);
         Assert::IsNull(result.activeState);
 
@@ -157,13 +154,13 @@ namespace UT {
 
       TEST_METHOD(Execute_ActiveStateOnTriggerWrong_UnhandledTransition)
       {
-        using namespace TriggerExecutorImpl;
+        using namespace EventDispatchersTestImpl;
 
         OnState on;
         Trigger::Wrong t;
 
         const int size = Length<TransitionList>::value;
-        auto result = Sm::TriggerExecutor<Trigger::Wrong, size - 1>().execute(&on, &t);
+        auto result = EventDispatcher<TransitionList, Trigger::Wrong, size - 1>().execute(&on, &t);
         Assert::IsFalse(result.consumed);
         Assert::IsNull(result.activeState);
 
@@ -173,13 +170,13 @@ namespace UT {
 
       TEST_METHOD(Execute_ActiveStateOffTriggerTimeout_CallsOffStateDoit)
       {
-        using namespace TriggerExecutorImpl;
+        using namespace EventDispatchersTestImpl;
 
         OffState off;
         Trigger::Timeout t;
 
         const int size = Length<TransitionList>::value;
-        auto result = Sm::TriggerExecutor<Trigger::Timeout, size - 1>::execute(&off, &t);
+        auto result = EventDispatcher<TransitionList, Trigger::Timeout, size - 1>::execute(&off, &t);
         Assert::IsTrue(result.consumed);
         Assert::AreEqual<int>(off.getTypeId(), result.activeState->getTypeId());
 
@@ -189,13 +186,13 @@ namespace UT {
 
       TEST_METHOD(Execute_ActiveStateOffTriggerOff_UnhandledTransition)
       {
-        using namespace TriggerExecutorImpl;
+        using namespace EventDispatchersTestImpl;
 
         OffState off;
         Trigger::Off t;
 
         const int size = Length<TransitionList>::value;
-        auto result = Sm::TriggerExecutor<Trigger::Off, size - 1>::execute(&off, &t);
+        auto result = EventDispatcher<TransitionList, Trigger::Off, size - 1>::execute(&off, &t);
         Assert::IsFalse(result.consumed);
         Assert::IsNull(result.activeState);
 
